d93e4e188efd06eff10a1b2ae879641071aba93b
DeloxyAdeola <108836569+DeloxyAdeola@users.noreply.github.com>
2022-10-10 19:14:09 -0400

    Add files via upload

--- Employees_Database_challenge.sql
+++ Employees_Database_challenge.sql
@@ -72,13 +72,4 @@ SELECT * FROM mentorship_eligibility;
 
 
 
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
